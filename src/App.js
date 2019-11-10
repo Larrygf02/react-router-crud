be351bfd7987a8f81ef9c2e621c6795bc2d416a3
larrygf02 <raulgf_02@hotmail.com>
2019-11-09 20:55:12 -0500

    Router components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Productos from './components/Productos'
+import EditarProducto from './components/EditarProducto'
+import AgregarProducto from './components/AgregarProducto'
+import Producto from './components/Producto'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/productos/nuevo" component={AgregarProducto}></Route>
+        <Route exact path="/productos" component={Productos}></Route>
+        <Route exact path="/productos/:id" component={Producto}></Route>
+        <Route exact path="/producto/editar/:id" component={EditarProducto}></Route>
+      </Switch>
+    </Router>
   );
 }
 
