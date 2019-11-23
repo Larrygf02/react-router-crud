94098dbb1fa71109513a7b21de187e268922b920
larrygf02 <raulgf_02@hotmail.com>
2019-11-23 18:16:14 -0500

    Generar formulario y state de

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 import Productos from './components/Productos'
 import EditarProducto from './components/EditarProducto'
 import AgregarProducto from './components/AgregarProducto'
@@ -7,13 +8,27 @@ import Producto from './components/Producto'
 import Header from './components/Header'
 
 function App() {
+  const [ productos, guardarProductos ] = useState([])
+  useEffect(() => {
+    const consultarApi = async () => {
+      const resultado = await axios.get('http://localhost:4000/restaurant')
+      console.log(resultado.data);
+      guardarProductos(resultado.data)
+    }
+    consultarApi()
+  }, [])
+
+  console.log(productos);
   return (
     <Router>
       <Header />
       <main className="container mt-5">
         <Switch>
+          <Route exact path="/productos" render={() => (
+              <Productos productos={productos}/>
+            )
+          }></Route>
           <Route exact path="/nuevo-producto" component={AgregarProducto}></Route>
-          <Route exact path="/productos" component={Productos}></Route>
           <Route exact path="/productos/:id" component={Producto}></Route>
           <Route exact path="/producto/editar/:id" component={EditarProducto}></Route>
         </Switch>
---
+++ src/components/ProductoLista.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function ProductoLista({producto}) {
+
+    const eliminarProducto = id => {
+        console.log('eliminando', id);
+        //TODO: Eliminar registrs
+    }
+
+    return (
+        <li data-categoria={producto.categoria} className="list-group-item d-flex justify-content-between align-items-center">
+            <p>
+                {producto.nombrePlatillo} {' '} 
+                <span className="font-weight-bold">${producto.precioPlatillo}</span>
+            </p>
+            <div>
+                <Link to={`/producto/editar/${producto.id}`}
+                    className="btn btn-success mr-2">Editar</Link>
+                <button type="button"
+                        className="btn btn-danger"
+                        onClick={() => eliminarProducto(producto.id)}>
+                            Eliminar
+                        </button>
+            </div>
+        </li>
+    )
+}
+
+export default ProductoLista;
